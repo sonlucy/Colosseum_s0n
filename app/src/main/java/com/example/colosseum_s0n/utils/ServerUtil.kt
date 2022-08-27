016ce77c0111f498675811afb1377dba3e482c60
s0n <shson971029@naver.com>
2022-08-27 21:10:09 +0900

    로그인화면 기본 구조

--- app/src/main/java/com/example/colosseum_s0n/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum_s0n/utils/ServerUtil.kt
@@ -3,4 +3,14 @@ package com.example.colosseum_s0n.utils
 class ServerUtil {
 //    어떤 내용? -> 서버 연결 전담.
 
+    companion object {  //클래스 명 다음에 점(.)을 쓰면 바로 참조할수있음. 자바의 static과 비슷
+
+//        모든 기능의 기본이 되는 주소.
+        val BASE_URL = "http://53.180.52.26"
+
+//        로그인 하는 기능
+        fun postRequestLogin() {  //로그인 하러 POST로 가는 요청.
+
+        }
+    }
 }
