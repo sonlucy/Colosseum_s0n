c4ef700a5591263c6d8167fbc0c7cebefed8fc99
s0n <shson971029@naver.com>
2022-08-27 21:28:54 +0900

    로그인 버튼 눌리면 -> 입력한 아이디와 비밀번호 가져오기

--- app/src/main/java/com/example/colosseum_s0n/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_s0n/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.colosseum_s0n
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,16 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+
+//            입력한 이메일,비밀번호가 뭔지 변수에 저장.
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+//            서버에 실제 회원이 맞는지 확인 요청. (Request)
+
+        }
 
     }
 
